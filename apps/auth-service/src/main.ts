a88836b8021c8a14e9cb3e6aaa9e42a686c172b4
Thomas Kortyka <info@toxictoast.de>
2024-12-04 13:57:45 +0100

    feat: configure auth-service

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,21 +1,45 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { DynamicModule, INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { connectKafka } from '@toxictoast/sleepyazkaban-kafka';
 
-async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	const globalPrefix = 'api';
-	app.setGlobalPrefix(globalPrefix);
-	const port = process.env.PORT || 3000;
-	await app.listen(port);
-	Logger.log(
-		`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-	);
+import {
+	configureApp,
+	configureFilters,
+	configureInterceptors,
+	configureCors,
+	configureValidation,
+} from '@toxictoast/sleepyazkaban-base-helpers';
+
+async function createApp(): Promise<INestApplication> {
+	return await NestFactory.create(AppModule);
 }
 
-bootstrap();
+async function startApp(app: INestApplication): Promise<void> {
+	const port = process.env.PORT ?? 3000;
+	await app.startAllMicroservices();
+	await app.listen(port);
+	Logger.log(`🚀 Listening on Port: ${port}`);
+}
+
+async function bootstrap() {
+	const globalPrefix = process.env.API_PREFIX ?? 'api';
+	const brokerHost = process.env.BROKER_HOST;
+	const brokerPort = parseInt(process.env.BROKER_PORT);
+	//
+	const app = await createApp();
+	await connectKafka(app, brokerHost, brokerPort);
+	configureApp(app, globalPrefix);
+	configureFilters(app);
+	configureInterceptors(app);
+	configureCors(app, ['http://localhost:3000']);
+	configureValidation(app, AppModule as unknown as DynamicModule);
+	await startApp(app);
+}
+
+bootstrap()
+	.catch((err) => Logger.error(err))
+	.then(() => {
+		Logger.log(`🚀 Auth is running`);
+		Logger.log(`🚀 Version: ${process.env.APP_VERSION}`);
+	});
