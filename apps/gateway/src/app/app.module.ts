358f302949fccee5d0de9a6f16c9823e1530f904
Thomas Kortyka <info@toxictoast.de>
2025-01-12 01:30:55 +0100

    chore: add health module

--- apps/gateway/src/app/app.module.ts
+++ apps/gateway/src/app/app.module.ts
@@ -1,26 +1,7 @@
 import { Module } from '@nestjs/common';
-import { HealthModule } from '@azkaban/shared';
+import { HealthModule } from './health/health.module';
 
 @Module({
-	imports: [
-		HealthModule.forRoot(
-			false,
-			{
-				memoryRSSTreshold: Number(process.env.MEMORY_RSS_TRESHOLD),
-				memoryHeapTreshold: Number(process.env.MEMORY_HEAP_TRESHOLD),
-			},
-			{
-				brokerHost: process.env.BROKER_HOST,
-				brokerPort: Number(process.env.BROKER_PORT),
-				brokerUsername: process.env.BROKER_USERNAME,
-				brokerPassword: process.env.BROKER_PASSWORD,
-			},
-			{
-				redisHost: process.env.REDIS_HOST,
-				redisPort: Number(process.env.REDIS_PORT),
-				redisPassword: process.env.REDIS_PASSWORD,
-			},
-		),
-	],
+	imports: [HealthModule],
 })
 export class AppModule {}
---
+++ apps/gateway/src/app/health/health.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { HealthModule as BaseModule } from '@azkaban/shared';
+
+@Module({
+	imports: [
+		BaseModule.forRoot(
+			false,
+			{
+				memoryRSSTreshold: Number(process.env.MEMORY_RSS_TRESHOLD),
+				memoryHeapTreshold: Number(process.env.MEMORY_HEAP_TRESHOLD),
+			},
+			{
+				brokerHost: process.env.BROKER_HOST,
+				brokerPort: Number(process.env.BROKER_PORT),
+				brokerUsername: process.env.BROKER_USERNAME,
+				brokerPassword: process.env.BROKER_PASSWORD,
+			},
+			{
+				redisHost: process.env.REDIS_HOST,
+				redisPort: Number(process.env.REDIS_PORT),
+				redisPassword: process.env.REDIS_PASSWORD,
+			},
+		),
+	],
+})
+export class HealthModule {}
