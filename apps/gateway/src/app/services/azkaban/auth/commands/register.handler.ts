6f62ee91939a29c6dfecad9c08d0d77fa4629349
Thomas Kortyka <t.kortyka@gmx.de>
2025-01-24 10:53:12 +0100

    chore: remove unused eventbus

--- apps/gateway/src/app/services/azkaban/auth/commands/register.handler.ts
+++ apps/gateway/src/app/services/azkaban/auth/commands/register.handler.ts
@@ -1,4 +1,4 @@
-import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { RegisterCommand } from './register.command';
 import { HttpException, Inject } from '@nestjs/common';
 import { ClientKafka } from '@nestjs/microservices';
