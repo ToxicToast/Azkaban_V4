7c6d01ce44158fd436a13a5bb8cebac7c07797ea
Thomas Kortyka <t.kortyka@gmx.de>
2025-01-29 15:00:50 +0100

    chore: fix origin

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -34,7 +34,7 @@ function addModules(app: INestApplication): void {
 
 function configureCors(app: INestApplication): void {
 	app.enableCors({
-		origin: ['localhost', 'version.toxictoast.de'],
+		origin: ['http://localhost', 'https://version.toxictoast.de'],
 		maxAge: 3600,
 		optionsSuccessStatus: 200,
 	});
