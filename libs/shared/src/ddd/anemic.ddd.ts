95b679888f25bbc29eb7dc74367eb2265dd4d558
Thomas Kortyka <t.kortyka@gmx.de>
2025-03-04 15:43:56 +0100

    refactor: remove isUpdated and isDeleted from anemic interface

--- libs/shared/src/ddd/anemic.ddd.ts
+++ libs/shared/src/ddd/anemic.ddd.ts
@@ -5,6 +5,4 @@ export interface Anemic {
 	readonly created_at: Date;
 	readonly updated_at: Nullable<Date>;
 	readonly deleted_at: Nullable<Date>;
-	readonly isUpdated: boolean;
-	readonly isDeleted: boolean;
 }
