0a2a46862cc6548bae1933885c5ef77f1de69a44
Thomas Kortyka <t.kortyka@gmx.de>
2025-03-18 11:19:14 +0100

    chore: add blizzard-api-service

---
+++ apps/blizzard-api-service/src/main.ts
@@ -0,0 +1,83 @@
+import { TelemetryHelper } from '@azkaban/shared';
+import { INestApplication, Logger, VersioningType } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AppConfig } from './config';
+
+const telemetry = TelemetryHelper(
+	AppConfig.telemetry,
+	AppConfig.name,
+	AppConfig.environment,
+);
+
+async function createApp(): Promise<INestApplication> {
+	return await NestFactory.create<INestApplication>(AppModule);
+}
+
+async function createMicroservice(app: INestApplication): Promise<void> {
+	const brokerUrl = `${AppConfig.broker.brokerHost}:${AppConfig.broker.brokerPort}`;
+	const brokerUsername = AppConfig.broker.brokerUsername;
+	const brokerPassword = AppConfig.broker.brokerPassword;
+	const environment = AppConfig.environment;
+	//
+	app.connectMicroservice<MicroserviceOptions>({
+		transport: Transport.KAFKA,
+		options: {
+			client: {
+				clientId: AppConfig.name,
+				brokers: [brokerUrl],
+				sasl:
+					environment !== 'local'
+						? {
+								mechanism: 'plain',
+								username: brokerUsername,
+								password: brokerPassword,
+							}
+						: undefined,
+				connectionTimeout: 4000,
+				authenticationTimeout: 4000,
+			},
+			consumer: {
+				groupId: AppConfig.name + '-consumer',
+			},
+		},
+	});
+}
+
+function configureApp(app: INestApplication): void {
+	const globalPrefix = 'api';
+	app.setGlobalPrefix(globalPrefix);
+	app.enableShutdownHooks();
+	app.enableVersioning({
+		type: VersioningType.URI,
+	});
+}
+
+async function startApp(app: INestApplication): Promise<void> {
+	const port = AppConfig.port;
+	await app.startAllMicroservices();
+	await app.listen(port);
+	Logger.log(`🚀 Listening on Port: ${port}`);
+}
+
+async function bootstrap() {
+	if (AppConfig.environment !== 'local') {
+		telemetry.start();
+	}
+	const app = await createApp();
+	configureApp(app);
+	await createMicroservice(app);
+	await startApp(app);
+	Logger.log(`🚀 User-Service is running`);
+	Logger.log(`🚀 Version: ${AppConfig.environment}`);
+}
+bootstrap().catch((err) => {
+	Logger.error(err);
+	if (AppConfig.environment !== 'local') {
+		telemetry
+			.shutdown()
+			.then(() => Logger.log('Tracing terminated'))
+			.catch((error) => Logger.error('Error terminating tracing', error));
+	}
+});
