4f9c9390df6b4ca4ce984900fb26ce47160dc9b8
Thomas Kortyka <t.kortyka@gmx.de>
2025-03-27 10:23:56 +0100

    fix: add service to module

--- apps/cronjob-service/src/app/cronjob/warcraft/inset/inset.module.ts
+++ apps/cronjob-service/src/app/cronjob/warcraft/inset/inset.module.ts
@@ -4,10 +4,16 @@ import { InsetController } from './inset.controller';
 import { InsetCron } from './inset.cron';
 import { InsetService } from './inset.service';
 import { InsetProcessor } from './inset.processor';
+import { DatabaseCharactersService } from '../services/character.service';
 
 @Module({
 	imports: [BullModule.registerQueue('blizzard-inset')],
 	controllers: [InsetController],
-	providers: [InsetService, InsetCron, InsetProcessor],
+	providers: [
+		InsetService,
+		DatabaseCharactersService,
+		InsetCron,
+		InsetProcessor,
+	],
 })
 export class InsetModule {}
--- apps/cronjob-service/src/app/cronjob/warcraft/mythic/mythic.module.ts
+++ apps/cronjob-service/src/app/cronjob/warcraft/mythic/mythic.module.ts
@@ -4,10 +4,16 @@ import { MythicController } from './mythic.controller';
 import { DatabaseCharactersService } from '../services/character.service';
 import { MythicCron } from './mythic.cron';
 import { MythicProcessor } from './mythic.processor';
+import { MythicService } from './mythic.service';
 
 @Module({
 	imports: [BullModule.registerQueue('blizzard-mythic')],
 	controllers: [MythicController],
-	providers: [DatabaseCharactersService, MythicCron, MythicProcessor],
+	providers: [
+		MythicService,
+		DatabaseCharactersService,
+		MythicCron,
+		MythicProcessor,
+	],
 })
 export class MythicModule {}
