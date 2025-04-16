a3b33bcc55e335927d6979aa43b55f7ca224fc48
Thomas Kortyka <t.kortyka@gmx.de>
2025-04-16 11:37:57 +0200

    fix: add webhook topics

--- libs/shared/src/topics/azkaban/webhook.topics.ts
+++ libs/shared/src/topics/azkaban/webhook.topics.ts
@@ -1,5 +1,6 @@
 export enum AzkabanWebhookTopics {
 	VERSION = 'azkaban.webhook.version',
+	APIALERTS = 'azkaban.webhook.apialerts',
 }
 
 export const AzkabanWebhookTopicArray = Object.values(AzkabanWebhookTopics);
