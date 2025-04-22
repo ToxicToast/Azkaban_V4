c2a0947bfe6b021ec86a0cc4297e8ab0007a1b65
Thomas Kortyka <t.kortyka@gmx.de>
2025-04-22 15:30:38 +0200

    fix: groups controller

--- apps/azkaban-service/src/app/groups/groups.controller.ts
+++ apps/azkaban-service/src/app/groups/groups.controller.ts
@@ -8,63 +8,63 @@ export class GroupsController {
 	@Span(AzkabanGroupTopics.LIST + '.service')
 	@MessagePattern(AzkabanGroupTopics.LIST)
 	async getGroupList(@Payload() payload: unknown) {
-		Logger.debug('Fetch Group List', payload);
+		Logger.log('Fetch Group List', payload);
 		return [];
 	}
 
 	@Span(AzkabanGroupTopics.ID + '.service')
 	@MessagePattern(AzkabanGroupTopics.ID)
 	async getGroupById(@Payload() payload: unknown) {
-		Logger.debug('Fetch Group By Id', payload);
+		Logger.log('Fetch Group By Id', payload);
 		return null;
 	}
 
-	@Span(AzkabanGroupTopics.USERID + '.service')
-	@MessagePattern(AzkabanGroupTopics.USERID)
+	@Span(AzkabanGroupTopics.GROUPID + '.service')
+	@MessagePattern(AzkabanGroupTopics.GROUPID)
 	async getGroupByGroupId(@Payload() payload: unknown) {
-		Logger.debug('Fetch Group By Group Id', payload);
+		Logger.log('Fetch Group By Group Id', payload);
 		return null;
 	}
 
 	@Span(AzkabanGroupTopics.CREATE + '.service')
 	@MessagePattern(AzkabanGroupTopics.CREATE)
 	async createGroup(@Payload() payload: unknown) {
-		Logger.debug('Create New Group', payload);
+		Logger.log('Create New Group', payload);
 		return null;
 	}
 
 	@Span(AzkabanGroupTopics.UPDATE + '.service')
 	@MessagePattern(AzkabanGroupTopics.UPDATE)
 	async updateGroup(@Payload() payload: unknown) {
-		Logger.debug('Update Group', payload);
+		Logger.log('Update Group', payload);
 		return null;
 	}
 
 	@Span(AzkabanGroupTopics.DELETE + '.service')
 	@MessagePattern(AzkabanGroupTopics.DELETE)
 	async deleteGroup(@Payload() payload: unknown) {
-		Logger.debug('Delete Group', payload);
+		Logger.log('Delete Group', payload);
 		return null;
 	}
 
 	@Span(AzkabanGroupTopics.RESTORE + '.service')
 	@MessagePattern(AzkabanGroupTopics.RESTORE)
 	async restoreGroup(@Payload() payload: unknown) {
-		Logger.debug('Restore Group', payload);
+		Logger.log('Restore Group', payload);
 		return null;
 	}
 
 	@Span(AzkabanGroupTopics.ACTIVATE + '.service')
 	@MessagePattern(AzkabanGroupTopics.ACTIVATE)
 	async activateGroup(@Payload() payload: unknown) {
-		Logger.debug('Activate Group', payload);
+		Logger.log('Activate Group', payload);
 		return null;
 	}
 
 	@Span(AzkabanGroupTopics.DEACTIVATE + '.service')
 	@MessagePattern(AzkabanGroupTopics.DEACTIVATE)
 	async deactivateGroup(@Payload() payload: unknown) {
-		Logger.debug('Deactivate Group', payload);
+		Logger.log('Deactivate Group', payload);
 		return null;
 	}
 }
