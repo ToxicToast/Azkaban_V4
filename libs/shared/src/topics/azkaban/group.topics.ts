6557be32ff50e22d9956869a5a0bd5aa7e69c055
Thomas Kortyka <t.kortyka@gmx.de>
2025-04-22 15:27:38 +0200

    fix: group topics

--- libs/shared/src/topics/azkaban/group.topics.ts
+++ libs/shared/src/topics/azkaban/group.topics.ts
@@ -1,5 +1,13 @@
 export enum AzkabanGroupTopics {
-	VERSION = 'azkaban.group.version',
+	LIST = 'azkaban.group.list',
+	ID = 'azkaban.group.id',
+	GROUPID = 'azkaban.group.groupId',
+	CREATE = 'azkaban.group.create',
+	UPDATE = 'azkaban.group.update',
+	DELETE = 'azkaban.group.delete',
+	RESTORE = 'azkaban.group.restore',
+	ACTIVATE = 'azkaban.group.activate',
+	DEACTIVATE = 'azkaban.group.deactivate',
 }
 
 export const AzkabanGroupTopicArray = Object.values(AzkabanGroupTopics);
