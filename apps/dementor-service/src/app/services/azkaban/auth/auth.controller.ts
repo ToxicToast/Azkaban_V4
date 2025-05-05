adb2b88e03d8e46507697c87b544c5aa49847858
Thomas Kortyka <t.kortyka@gmx.de>
2025-05-05 20:10:58 +0200

    feat: add login command

---
+++ apps/dementor-service/src/app/services/azkaban/auth/auth.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { AzkabanAuthTopics, ControllerHelper } from '@azkaban/shared';
+import { Span } from 'nestjs-otel';
+import { AuthService } from './auth.service';
+
+@Controller(ControllerHelper('azkaban/auth'))
+export class AuthController {
+	constructor(private readonly service: AuthService) {}
+
+	@Span(AzkabanAuthTopics.LOGIN + '.dementor')
+	@Post('/login')
+	async login(@Body() body: { username: string; password: string }) {
+		Logger.log('Login User', { body });
+		return await this.service.login(body);
+	}
+}
---
+++ apps/dementor-service/src/app/services/azkaban/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { CommandBus } from '@nestjs/cqrs';
+import { Span } from 'nestjs-otel';
+import { AzkabanAuthTopics } from '@azkaban/shared';
+import { LoginCommand } from './commands';
+
+@Injectable()
+export class AuthService {
+	constructor(private readonly commandBus: CommandBus) {}
+
+	@Span(AzkabanAuthTopics.LOGIN + '.dementor')
+	async login(data: { username: string; password: string }) {
+		Logger.log('Login User', { data });
+		return await this.commandBus.execute(new LoginCommand(data));
+	}
+}
