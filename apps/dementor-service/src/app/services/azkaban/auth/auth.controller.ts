6db0c53e112919fe7712648a3f7f34b7b234e859
Thomas Kortyka <t.kortyka@gmx.de>
2025-05-14 23:32:17 +0200

    fix: unknown strategy

--- apps/dementor-service/src/app/services/azkaban/auth/auth.controller.ts
+++ apps/dementor-service/src/app/services/azkaban/auth/auth.controller.ts
@@ -16,7 +16,6 @@ export class AuthController {
 	constructor(private readonly service: AuthService) {}
 
 	@Span(AzkabanAuthTopics.LOGIN + '.dementor')
-	@UseGuards(LocalAuthGuard)
 	@Post('/login')
 	async login(
 		@Body() body: { username: string; password: string },
