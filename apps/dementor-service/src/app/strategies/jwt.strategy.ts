b6e51b8f996bf7f57dea5ab29c791d58a9805358
Thomas Kortyka <t.kortyka@gmx.de>
2025-05-19 17:58:16 +0200

    fix: check for JWT Date

--- apps/dementor-service/src/app/strategies/jwt.strategy.ts
+++ apps/dementor-service/src/app/strategies/jwt.strategy.ts
@@ -15,8 +15,15 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 		});
 	}
 
-	async validate(payload: Chainable<Partial<UserDAO>, { sub: string }>) {
+	async validate(
+		payload: Chainable<Partial<UserDAO>, { sub: string; exp: number }>,
+	) {
 		Logger.log('Validating JWT', { payload });
+		const timestampNow = Math.floor(Date.now() / 1000);
+		if (payload.exp < timestampNow) {
+			Logger.warn('JWT expired', { payload });
+			return null;
+		}
 		return {
 			id: payload.id,
 			username: payload.username,
