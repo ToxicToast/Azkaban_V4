f6eb8d57ea1c36a3408a0b0cd4e10bde4b03740f
Thomas Kortyka <t.kortyka@gmx.de>
2025-05-19 13:26:48 +0200

    fix: add query handler

--- apps/dementor-service/src/app/services/warhammer/character/character.module.ts
+++ apps/dementor-service/src/app/services/warhammer/character/character.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { CharacterController } from './character.controller';
 import { CharacterService } from './character.service';
+import {
+	CharacterIdHandler,
+	IdQueryHandler,
+	ListQueryHandler,
+} from './queries';
 
 @Module({
 	controllers: [CharacterController],
-	providers: [CharacterService],
+	providers: [
+		CharacterService,
+		ListQueryHandler,
+		IdQueryHandler,
+		CharacterIdHandler,
+	],
 })
 export class CharacterModule {}
