73af1285496403c4319e08600ab58cea04d81eef
Thomas Kortyka <t.kortyka@gmx.de>
2025-05-19 19:32:28 +0200

    fix: character domain

--- libs/warhammer-domain/src/domains/character.domain.ts
+++ libs/warhammer-domain/src/domains/character.domain.ts
@@ -21,9 +21,19 @@ export class CharacterDomain
 		private readonly character_id: string,
 		private readonly name: string,
 		private readonly role: string,
-		private fate: Fate,
-		private wounds: Wound,
-		private corruption: Corruption,
+		private fate: {
+			current: number;
+			total: number;
+		},
+		private wounds: {
+			current: number;
+			total: number;
+			critical: number;
+		},
+		private corruption: {
+			current: number;
+			total: number;
+		},
 		private activated_at: Nullable<Date>,
 		private readonly created_at: Date,
 		private updated_at: Nullable<Date>,
@@ -38,19 +48,9 @@ export class CharacterDomain
 			character_id: this.character_id,
 			name: this.name,
 			role: this.role,
-			fate: {
-				current: this.fate.getCurrentFate(),
-				total: this.fate.getTotalFate(),
-			},
-			wounds: {
-				current: this.wounds.getCurrentWounds(),
-				total: this.wounds.getTotalWounds(),
-				critical: this.wounds.getCriticalWounds(),
-			},
-			corruption: {
-				current: this.corruption.getCurrentCorruption(),
-				total: this.corruption.getTotalCorruption(),
-			},
+			fate: this.fate,
+			wounds: this.wounds,
+			corruption: this.corruption,
 			activated_at: this.activated_at,
 			created_at: this.created_at,
 			updated_at: this.updated_at,
@@ -69,88 +69,72 @@ export class CharacterDomain
 	}
 
 	healWound(wound: number) {
-		if (!this.wounds.equals(wound)) {
-			this.updated_at = new Date();
-			const oldWounds = this.wounds.getCurrentWounds();
-			this.wounds.healWounds(wound);
-			this.addDomainEvent(
-				new HealWoundEvent(this.character_id, wound, oldWounds),
-			);
-		}
+		this.updated_at = new Date();
+		const oldWounds = this.wounds.current;
+		this.wounds.current = oldWounds - wound;
+		this.addDomainEvent(
+			new HealWoundEvent(this.character_id, wound, oldWounds),
+		);
 	}
 
 	addWound(wound: number) {
-		if (!this.wounds.equals(wound)) {
-			this.updated_at = new Date();
-			const oldWounds = this.wounds.getCurrentWounds();
-			const oldCritical = this.wounds.getCriticalWounds();
-			this.wounds.addWounds(wound);
+		this.updated_at = new Date();
+		const oldWounds = this.wounds.current;
+		const oldCritical = this.wounds.critical;
+		this.wounds.current = oldWounds + wound;
+		this.addDomainEvent(
+			new AddWoundEvent(this.character_id, wound, oldWounds),
+		);
+		if (this.wounds.current > this.wounds.total) {
+			this.wounds.critical = oldCritical + 1;
 			this.addDomainEvent(
-				new AddWoundEvent(this.character_id, wound, oldWounds),
+				new CriticalWoundEvent(
+					this.character_id,
+					oldCritical,
+					this.wounds.critical,
+				),
 			);
-			if (this.wounds.isCriticalWound()) {
-				this.wounds.inflictCriticalWound();
-				this.addDomainEvent(
-					new CriticalWoundEvent(
-						this.character_id,
-						oldCritical,
-						this.wounds.getCriticalWounds(),
-					),
-				);
-			}
 		}
 	}
 
 	addFate(fate: number) {
-		if (!this.fate.equals(fate)) {
-			this.updated_at = new Date();
-			const oldFate = this.fate.getCurrentFate();
-			this.fate.addFate(fate);
-			this.addDomainEvent(
-				new AddFateEvent(this.character_id, fate, oldFate),
-			);
-		}
+		this.updated_at = new Date();
+		const oldFate = this.fate.current;
+		this.fate.current = fate;
+		this.addDomainEvent(new AddFateEvent(this.character_id, fate, oldFate));
 	}
 
 	useFate(fate: number) {
-		if (!this.fate.equals(fate)) {
-			this.updated_at = new Date();
-			const oldFate = this.fate.getCurrentFate();
-			this.fate.useFate(fate);
-			this.addDomainEvent(
-				new UseFateEvent(this.character_id, fate, oldFate),
-			);
-		}
+		this.updated_at = new Date();
+		const oldFate = this.fate.current;
+		this.fate.current = oldFate - fate;
+		this.addDomainEvent(new UseFateEvent(this.character_id, fate, oldFate));
 	}
 
 	addCorruption(corruption: number) {
-		if (!this.corruption.equals(corruption)) {
-			this.updated_at = new Date();
-			const oldCorruption = this.corruption.getCurrentCorruption();
-			this.corruption.addCorruption(corruption);
-			this.addDomainEvent(
-				new AddCorruptionEvent(
-					this.character_id,
-					corruption,
-					oldCorruption,
-				),
-			);
-		}
+		this.updated_at = new Date();
+		const oldCorruption = this.corruption.current;
+		this.corruption.current = this.corruption.current + corruption;
+		this.addDomainEvent(
+			new AddCorruptionEvent(
+				this.character_id,
+				corruption,
+				oldCorruption,
+			),
+		);
 	}
 
 	cleanseCorruption(corruption: number) {
-		if (!this.corruption.equals(corruption)) {
-			this.updated_at = new Date();
-			const oldCorruption = this.corruption.getCurrentCorruption();
-			this.corruption.cleanseCorruption(corruption);
-			this.addDomainEvent(
-				new CleanseCorruptionEvent(
-					this.character_id,
-					corruption,
-					oldCorruption,
-				),
-			);
-		}
+		this.updated_at = new Date();
+		const oldCorruption = this.corruption.current;
+		this.corruption.current = this.corruption.current - corruption;
+		this.addDomainEvent(
+			new CleanseCorruptionEvent(
+				this.character_id,
+				corruption,
+				oldCorruption,
+			),
+		);
 	}
 
 	activateCharacter(): void {
