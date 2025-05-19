a7fae9188f2f5a35a009e30b7880c5dae9a96669
Thomas Kortyka <t.kortyka@gmx.de>
2025-05-19 13:46:39 +0200

    fix: add warhammer topics

--- apps/dementor-service/src/app/app.module.ts
+++ apps/dementor-service/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {
 	AzkabanTopicArray,
 	AzkabanAuthTopicArray,
 	WarhammerTopicArray,
+	WarhammerCharacterTopicArray,
 } from '@azkaban/shared';
 import { AppConfig } from '../config';
 import { CqrsModule } from '@nestjs/cqrs';
@@ -75,6 +76,7 @@ import { PassportModule } from '@nestjs/passport';
 				...WarcraftCharacterTopicArray,
 				...WarcraftGuildTopicArray,
 				// Warhammer Services
+				...WarhammerCharacterTopicArray,
 				// Versioning
 				...WarcraftTopicArray,
 				...AzkabanTopicArray,
