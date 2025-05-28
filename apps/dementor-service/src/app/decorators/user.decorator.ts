f8d2c5f2c198616e188754d0aca862e32bc5a664
Thomas Kortyka <t.kortyka@gmx.de>
2025-05-28 16:16:16 +0200

    chore: remove logging from decorator

--- apps/dementor-service/src/app/decorators/user.decorator.ts
+++ apps/dementor-service/src/app/decorators/user.decorator.ts
@@ -2,7 +2,6 @@ import { createParamDecorator, ExecutionContext, Logger } from '@nestjs/common';
 
 export const User = createParamDecorator(
 	(data: unknown, ctx: ExecutionContext) => {
-		Logger.log('User decorator called', { data });
 		const request = ctx.switchToHttp().getRequest();
 		if (!request.user) {
 			Logger.error('User not found in request');
